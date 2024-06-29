b099683d588ae067a37acd0794dc9c8e7f86f3e4
Daniyar Yersultanov <serani541@gmail.com>
2024-06-29 22:38:21 +0500

    Basic games integration

---
+++ webproject/src/components/Timer.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+const Timer = ({isActive, onTimeUp}) => {
+    const [time, setTime] = useState(() => {
+        const savedTime = localStorage.getItem('time');
+        return savedTime ? parseInt(savedTime, 10) : 600;
+    });
+
+    useEffect(() => {
+        let interval;
+        if (isActive) {
+            interval = setInterval(() => {
+                setTime((prevTime) => {
+                    if (prevTime <= 1) {
+                        clearInterval(interval);
+                        localStorage.removeItem('time');
+                        onTimeUp();
+                        return 0;
+                    }
+                    const newTime = prevTime - 1;
+                    localStorage.setItem('time', newTime);
+                    return newTime;
+                });
+            }, 1000);
+        } else if (!isActive && time !== 0) {
+            clearInterval(interval);
+        }
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [isActive, onTimeUp, time]);
+
+    return (
+        <div>
+            <h1>Time: {Math.floor(time / 60)}:{String(time % 60).padStart(2, '0')}</h1>
+        </div>
+    );
+}
+
+export default Timer;
--- webproject/src/pages/dashboard_sections/Games.jsx
+++ webproject/src/pages/dashboard_sections/Games.jsx
@@ -1,11 +1,100 @@
 import "/src/dashboard.css";
 import Sidebar from "../Sidebar";
 import Navdash from "../Navdash";
+import { useEffect, useRef, useState } from "react";
+import { 
+  playGame,
+  fetchUserData      
+ } from "../../utils/apiService";
+ import Timer from "../../components/Timer";
 
 const Games = () => {
-  const openGameWindow = (path) => {
-    window.open(path, "_blank", "width=800,height=600");
-  }
+
+  const [user, setUser] = useState({});
+  const [isChild, setIsChild] = useState(false);
+  const [childId, setChildId] = useState("");
+  const [open, setOpen] = useState(false);
+  const externalWindow = useRef(null);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const childId = localStorage.getItem("childId");
+        let userData;
+        if (childId) {
+          userData = await fetchUserData(childId);
+          setIsChild(true);
+          setChildId(childId);
+
+        } else {
+          userData = await fetchUserData();
+        }
+        setUser(userData);
+      } catch (error) {
+        console.error("Error loading data:", error);
+      }
+    };
+    loadData();
+  }, []);
+
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+        if (externalWindow.current) {
+            externalWindow.current.close();
+        }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+        window.removeEventListener("beforeunload", handleBeforeUnload);
+        if (externalWindow.current) {
+            externalWindow.current.close();
+        }
+    };
+  }, []);
+
+  const fetchChildData = async () => {
+    try {
+      const updatedUserData = await fetchUserData(childId);
+      setUser(updatedUserData);
+    } catch (error) {
+      console.error("Error fetching updated child data:", error);
+    }
+  };
+
+  const openGameWindow = async (path) => {
+
+    if (!open) {
+      const savedTime = localStorage.getItem('time');
+      if (!savedTime || parseInt(savedTime, 10) <= 0) {
+        localStorage.setItem('time', 600);
+      }
+      externalWindow.current = window.open(path, "_blank", "width=800,height=600");
+      setOpen(true);
+      
+      const ifWindowClosed = () => {
+        if (externalWindow.current && externalWindow.current.closed) {
+          setOpen(false);
+          console.log("Game window closed");
+        } else {
+          setTimeout(ifWindowClosed, 10);
+        }
+      };
+
+      ifWindowClosed();
+    }
+  };
+
+  const handleTimeUp = () => {
+    if (externalWindow.current) {
+      externalWindow.current.close();
+    }
+    setOpen(false);
+    playGame(childId);
+    fetchChildData();
+  };
+
   return (
     <div className="rtdash">
       <Sidebar />
@@ -13,6 +102,7 @@ const Games = () => {
         <Navdash />
         <div>
           <button onClick={() => openGameWindow("/src/pages/dashboard_sections/games/3ryad/index.html")}>Open Game</button>
+          {open && <Timer isActive={open} onTimeUp={handleTimeUp}/>}
         </div>
       </div>
     </div>
