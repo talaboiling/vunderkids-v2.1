c6a3f7f613f968134d9777b304b1124e7d1b6249
BekzhanK1 <bkimadieff@gmail.com>
2024-08-19 22:48:00 +0500

    fix: reverted

--- webproject/src/pages/courses/SectionContent.jsx
+++ webproject/src/pages/courses/SectionContent.jsx
@@ -22,94 +22,94 @@ const SectionContent = ({
       >
         {t("courseStart")}
       </h2>
-        <div className="contWrapper">
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-            }}
-          >
-            <hr className="lessonsHr" />
-            <h2 className="defaultStyle" style={{ color: "#aaa" }}>
-              {section.title} {chapter.title}
-            </h2>
-            <hr className="lessonsHr" />
-          </div>
-          {chapter.contents.map((content, contentIndex) => (
-            <div className="lessonsLinks" key={contentIndex}>
-              {content.content_type === "lesson" ? (
-                <div
-                  className={`vidBlock studVidBlock ${
-                    hasSubscription ? "" : "noVidBlock"
-                  }`}
-                  onClick={() =>
-                    hasSubscription
-                      ? openVideoModal(content.video_url)
-                      : setShowSubscriptionError(true)
-                  }
-                >
-                  <div className="thumbcontainer">
-                    <img
-                      src={bgvideo || "placeholder.png"}
-                      alt="vidname"
-                      className="taskThumbnail"
-                    />
-                  </div>
-                  <p
-                    style={{
-                      backgroundColor: "white",
-                      margin: "0",
-                      padding: "7px 40px",
-                      borderRadius: "10px",
-                    }}
-                  >
-                    {content.title}
-                  </p>
-                </div>
-              ) : (
-                <div
-                  className={`studVidBlock task ${
-                    hasSubscription ? "" : "noTask"
-                  }`}
-                  onClick={() =>
-                    hasSubscription
-                      ? openTaskModal(content.chapter, content.id)
-                      : setShowSubscriptionError(true)
-                  }
-                >
+      <div className="contWrapper">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <hr className="lessonsHr" />
+          <h2 className="defaultStyle" style={{ color: "#aaa" }}>
+            {section.title} {chapter.title}
+          </h2>
+          <hr className="lessonsHr" />
+        </div>
+        {chapter.contents.map((content, contentIndex) => (
+          <div className="lessonsLinks" key={contentIndex}>
+            {content.content_type === "lesson" ? (
+              <div
+                className={`vidBlock studVidBlock ${
+                  hasSubscription ? "" : "noVidBlock"
+                }`}
+                onClick={() =>
+                  hasSubscription
+                    ? openVideoModal(content.video_url)
+                    : setShowSubscriptionError(true)
+                }
+              >
+                <div className="thumbcontainer">
                   <img
-                    src={bgtask || "placeholder.png"}
+                    src={bgvideo || "placeholder.png"}
                     alt="vidname"
                     className="taskThumbnail"
                   />
-                  <p
-                    style={{
-                      backgroundColor: "white",
-                      margin: "0",
-                      padding: "7px 40px",
-                      borderRadius: "10px",
-                      marginBottom: "7px",
-                    }}
-                  >
-                    {content.title}
-                  </p>
-                  {content.is_completed ? (
-                    <div className="completedTask">
-                      <VerifiedIcon sx={{ color: "#19a5fc" }} />
-                      <strong>{t("youCompletedTask")}</strong>
-                    </div>
-                  ) : (
-                    <div className="completedTask incompleteTask">
-                      <strong>{t("youHaveNewTask")}</strong>
-                    </div>
-                  )}
                 </div>
-              )}
-            </div>
-          ))}
-        </div>
-      
+                <p
+                  style={{
+                    backgroundColor: "white",
+                    margin: "0",
+                    padding: "7px 40px",
+                    borderRadius: "10px",
+                  }}
+                >
+                  {content.title}
+                </p>
+              </div>
+            ) : (
+              <div
+                className={`studVidBlock task ${
+                  hasSubscription ? "" : "noTask"
+                }`}
+                onClick={() =>
+                  hasSubscription
+                    ? openTaskModal(content.id)
+                    : setShowSubscriptionError(true)
+                }
+              >
+                <img
+                  src={bgtask || "placeholder.png"}
+                  alt="vidname"
+                  className="taskThumbnail"
+                />
+                <p
+                  style={{
+                    backgroundColor: "white",
+                    margin: "0",
+                    padding: "7px 40px",
+                    borderRadius: "10px",
+                    marginBottom: "7px",
+                  }}
+                >
+                  {content.title}
+                </p>
+                {content.is_completed ? (
+                  <div className="completedTask">
+                    <VerifiedIcon sx={{ color: "#19a5fc" }} />
+                    <strong>{t("youCompletedTask")}</strong>
+                  </div>
+                ) : (
+                  <div className="completedTask incompleteTask">
+                    <strong>{t("youHaveNewTask")}</strong>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+
       {showSubscriptionError && (
         <SubscriptionErrorModal
           setShowSubscriptionError={setShowSubscriptionError}
