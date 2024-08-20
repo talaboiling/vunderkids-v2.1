9f4770502ca89834a544778f7e52cd8416c6b756
BekzhanK1 <bkimadieff@gmail.com>
2024-08-20 13:31:54 +0500

    fix: fetchSection

--- webproject/src/pages/courses/ChapterContent.jsx
+++ webproject/src/pages/courses/ChapterContent.jsx
@@ -18,111 +18,133 @@ import {
 import { useTranslation } from "react-i18next";
 
 const ChapterContent = () => {
-    const { t } = useTranslation();
-    const { courseId, sectionId } = useParams();
-    const [user, setUser] = useState({ first_name: t("student"), last_name: "" }); // Default values
-    const [course, setCourse] = useState(); // State to store courses
-    const [section, setSection] = useState([]); // State to store sections
-    const [chapters, setChapters] = useState([]);
-    const [loading, setLoading] = useState(true); // Add loading state
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isProfileSwitched, setIsProfileSwitched] = useState(false);
-    const [activeSection, setIsActiveSection] = useState(null);
+  const { t } = useTranslation();
+  const { courseId, sectionId } = useParams();
+  const [user, setUser] = useState({ first_name: t("student"), last_name: "" }); // Default values
+  const [course, setCourse] = useState(); // State to store courses
+  const [section, setSection] = useState([]); // State to store sections
+  const [chapters, setChapters] = useState([]);
+  const [loading, setLoading] = useState(true); // Add loading state
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isProfileSwitched, setIsProfileSwitched] = useState(false);
+  const [activeSection, setIsActiveSection] = useState(null);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const childId = localStorage.getItem("child_id");
-        try {
-          // const [userData, coursesData, weeklyProgressData] = await [
-          //   fetchUserData(childId),
-          //   fetchCourses(childId),
-          //   fetchWeeklyProgress(childId),
-          // ];
-          const userData = await fetchUserData(childId);
-          console.log("userData", userData);
-          setUser(userData);
-          const courseData = await fetchCourse(courseId, childId);
-          setCourse(courseData);
-          const sectionData = await fetchSection(courseId, childId);
-          setSection(sectionData);
-          const chaptersData = await fetchChapters(courseId, sectionId, childId);
-          setChapters(chaptersData);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchData();
-    }, [courseId, sectionId]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const childId = localStorage.getItem("child_id");
+      try {
+        // const [userData, coursesData, weeklyProgressData] = await [
+        //   fetchUserData(childId),
+        //   fetchCourses(childId),
+        //   fetchWeeklyProgress(childId),
+        // ];
+        const userData = await fetchUserData(childId);
+        console.log("userData", userData);
+        setUser(userData);
+        const courseData = await fetchCourse(courseId, childId);
+        setCourse(courseData);
+        const sectionData = await fetchSection(courseId, sectionId, childId);
+        setSection(sectionData);
+        const chaptersData = await fetchChapters(courseId, sectionId, childId);
+        setChapters(chaptersData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    const handleClickSection = (sectionId) => {
-        if (activeSection !== sectionId) {
-          setIsActiveSection(sectionId);
-        }
+    fetchData();
+  }, [courseId, sectionId]);
+
+  const handleClickSection = (sectionId) => {
+    if (activeSection !== sectionId) {
+      setIsActiveSection(sectionId);
     }
-  
-    if (loading) {
-      return <Loader />; // Display loader while fetching data
-    }
+  };
+
+  if (loading) {
+    return <Loader />; // Display loader while fetching data
+  }
   return (
     <div className="rtdash dashMain">
-        <Sidebar isMenuOpen={isMenuOpen} />
-        <div className="centralDash">
-            <Navdash
-            starCount={user.stars}
-            cupCount={user.cups}
-            gradeNum={user.grade}
-            isMenuOpen={isMenuOpen}
-            setIsMenuOpen={setIsMenuOpen}
-            isProfileSwitched={isProfileSwitched}
-            setIsProfileSwitched={setIsProfileSwitched}
-            urlPath={"dashboard"}
-            />
-            <div className="mainContent">
-                <div className="coursesCards">
-                    <CourseCard course={course} t={t} />
+      <Sidebar isMenuOpen={isMenuOpen} />
+      <div className="centralDash">
+        <Navdash
+          starCount={user.stars}
+          cupCount={user.cups}
+          gradeNum={user.grade}
+          isMenuOpen={isMenuOpen}
+          setIsMenuOpen={setIsMenuOpen}
+          isProfileSwitched={isProfileSwitched}
+          setIsProfileSwitched={setIsProfileSwitched}
+          urlPath={"dashboard"}
+        />
+        <div className="mainContent">
+          <div className="coursesCards">
+            <CourseCard course={course} t={t} />
+          </div>
+          <div className="courseNavigation">
+            <Link to={`/dashboard/courses/${courseId}/sections`}>
+              <p
+                className="defaultStyle courseNav"
+                id={
+                  window.location.pathname ===
+                  `/dashboard/courses/${courseId}/sections`
+                    ? "active"
+                    : ""
+                }
+              >
+                Разделы
+              </p>
+            </Link>
+            <Link
+              to={`/dashboard/courses/${courseId}/sections/${sectionId}/chapters`}
+            >
+              <p
+                className="defaultStyle courseNav"
+                id={
+                  window.location.pathname ===
+                  `/dashboard/courses/${courseId}/sections/${sectionId}/chapters`
+                    ? "active"
+                    : ""
+                }
+              >
+                Темы
+              </p>
+            </Link>
+          </div>
+          <ul className="sectionsList">
+            {chapters.map((chapter) => (
+              <li
+                key={chapter.id}
+                className={`sectionItem ${
+                  activeSection === chapter.id ? "activeSection" : ""
+                }`}
+                onClick={() => handleClickSection(chapter.id)}
+              >
+                <p>{chapter.title}</p>
+                <div className="sectionProgress">
+                  <p className="defaultStyle">
+                    {t("completedTasks1")}
+                    {chapter.completed_tasks}
+                    {t("completedTasks2")}
+                    {chapter.total_tasks} {t("completedTasks3")}
+                  </p>
+                  <progress value={chapter.percentage_completed} />
                 </div>
-                <div className="courseNavigation">
-                    <Link to={`/dashboard/courses/${courseId}/sections`}>
-                        <p
-                            className="defaultStyle courseNav"
-                            id={window.location.pathname === `/dashboard/courses/${courseId}/sections` ? "active" : ""}>
-                                Разделы
-                        </p>
-                    </Link>
-                    <Link to={`/dashboard/courses/${courseId}/sections/${sectionId}/chapters`}>
-                        <p
-                            className="defaultStyle courseNav"
-                            id={window.location.pathname === `/dashboard/courses/${courseId}/sections/${sectionId}/chapters` ? "active" : ""}>
-                                Темы
-                        </p>
-                    </Link>
-                </div>
-                <ul className="sectionsList">
-                    {chapters.map((chapter) => (
-                        <li key={chapter.id} 
-                            className={`sectionItem ${activeSection === chapter.id ? "activeSection" : ""}`} 
-                            onClick={() => handleClickSection(chapter.id)}>
-                            <p>{chapter.title}</p>
-                            <div className="sectionProgress">
-                                <p className="defaultStyle">
-                                    {t("completedTasks1")}
-                                    {chapter.completed_tasks}
-                                    {t("completedTasks2")}
-                                    {chapter.total_tasks} {t("completedTasks3")}
-                                </p>
-                                <progress value={chapter.percentage_completed} />
-                            </div>
-                            <Link to={`/dashboard/courses/${courseId}/sections/${sectionId}/chapters/${chapter.id}/lessons`}>
-                                <button className="orangeButton"><PlayArrowIcon /></button>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                <Link
+                  to={`/dashboard/courses/${courseId}/sections/${sectionId}/chapters/${chapter.id}/lessons`}
+                >
+                  <button className="orangeButton">
+                    <PlayArrowIcon />
+                  </button>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
     </div>
   );
 };
--- webproject/src/pages/courses/CourseContent.jsx
+++ webproject/src/pages/courses/CourseContent.jsx
@@ -17,101 +17,115 @@ import {
 import { useTranslation } from "react-i18next";
 
 const CourseContent = () => {
-    const { t } = useTranslation();
-    const { courseId } = useParams();
-    const [user, setUser] = useState({ first_name: t("student"), last_name: "" }); // Default values
-    const [course, setCourse] = useState(); // State to store courses
-    const [sections, setSections] = useState([]); // State to store sections
-    const [loading, setLoading] = useState(true); // Add loading state
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isProfileSwitched, setIsProfileSwitched] = useState(false);
-    const [activeSection, setIsActiveSection] = useState(null);
+  const { t } = useTranslation();
+  const { courseId } = useParams();
+  const [user, setUser] = useState({ first_name: t("student"), last_name: "" }); // Default values
+  const [course, setCourse] = useState(); // State to store courses
+  const [sections, setSections] = useState([]); // State to store sections
+  const [loading, setLoading] = useState(true); // Add loading state
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isProfileSwitched, setIsProfileSwitched] = useState(false);
+  const [activeSection, setIsActiveSection] = useState(null);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const childId = localStorage.getItem("child_id");
-        try {
-          // const [userData, coursesData, weeklyProgressData] = await [
-          //   fetchUserData(childId),
-          //   fetchCourses(childId),
-          //   fetchWeeklyProgress(childId),
-          // ];
-          const userData = await fetchUserData(childId);
-          console.log("userData", userData);
-          setUser(userData);
-          const courseData = await fetchCourse(courseId, childId);
-          setCourse(courseData);
-          const sectionsData = await fetchSections(courseId, childId);
-          setSections(sectionsData);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchData();
-    }, [courseId]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const childId = localStorage.getItem("child_id");
+      try {
+        // const [userData, coursesData, weeklyProgressData] = await [
+        //   fetchUserData(childId),
+        //   fetchCourses(childId),
+        //   fetchWeeklyProgress(childId),
+        // ];
+        const userData = await fetchUserData(childId);
+        console.log("userData", userData);
+        setUser(userData);
+        const courseData = await fetchCourse(courseId, childId);
+        setCourse(courseData);
+        const sectionsData = await fetchSections(courseId, childId);
+        setSections(sectionsData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    const handleClickSection = (sectionId) => {
-        if (activeSection !== sectionId) {
-          setIsActiveSection(sectionId);
-        }
+    fetchData();
+  }, [courseId]);
+
+  const handleClickSection = (sectionId) => {
+    if (activeSection !== sectionId) {
+      setIsActiveSection(sectionId);
     }
-  
-    if (loading) {
-      return <Loader />; // Display loader while fetching data
-    }
+  };
+
+  if (loading) {
+    return <Loader />; // Display loader while fetching data
+  }
   return (
     <div className="rtdash dashMain">
-        <Sidebar isMenuOpen={isMenuOpen} />
-        <div className="centralDash">
-            <Navdash
-            starCount={user.stars}
-            cupCount={user.cups}
-            gradeNum={user.grade}
-            isMenuOpen={isMenuOpen}
-            setIsMenuOpen={setIsMenuOpen}
-            isProfileSwitched={isProfileSwitched}
-            setIsProfileSwitched={setIsProfileSwitched}
-            urlPath={"dashboard"}
-            />
-            <div className="mainContent">
-                <div className="coursesCards">
-                    <CourseCard course={course} t={t} />
+      <Sidebar isMenuOpen={isMenuOpen} />
+      <div className="centralDash">
+        <Navdash
+          starCount={user.stars}
+          cupCount={user.cups}
+          gradeNum={user.grade}
+          isMenuOpen={isMenuOpen}
+          setIsMenuOpen={setIsMenuOpen}
+          isProfileSwitched={isProfileSwitched}
+          setIsProfileSwitched={setIsProfileSwitched}
+          urlPath={"dashboard"}
+        />
+        <div className="mainContent">
+          <div className="coursesCards">
+            <CourseCard course={course} t={t} />
+          </div>
+          <div className="courseNavigation">
+            <Link to={`/dashboard/courses/${courseId}/sections`}>
+              <p
+                className="defaultStyle courseNav"
+                id={
+                  window.location.pathname ===
+                  `/dashboard/courses/${courseId}/sections`
+                    ? "active"
+                    : ""
+                }
+              >
+                Разделы
+              </p>
+            </Link>
+          </div>
+          <ul className="sectionsList">
+            {sections.map((section) => (
+              <li
+                key={section.id}
+                className={`sectionItem ${
+                  activeSection === section.id ? "activeSection" : ""
+                }`}
+                onClick={() => handleClickSection(section.id)}
+              >
+                <p>{section.title}</p>
+                <div className="sectionProgress">
+                  <p className="defaultStyle">
+                    {t("completedTasks1")}
+                    {section.completed_tasks}
+                    {t("completedTasks2")}
+                    {section.total_tasks} {t("completedTasks3")}
+                  </p>
+                  <progress value={section.percentage_completed} />
                 </div>
-                <div className="courseNavigation">
-                    <Link to={`/dashboard/courses/${courseId}/sections`}>
-                        <p
-                            className="defaultStyle courseNav"
-                            id={window.location.pathname === `/dashboard/courses/${courseId}/sections` ? "active" : ""}>
-                                Разделы
-                        </p>
-                    </Link>
-                </div>
-                <ul className="sectionsList">
-                    {sections.map((section) => (
-                        <li key={section.id} 
-                            className={`sectionItem ${activeSection === section.id ? "activeSection" : ""}`} 
-                            onClick={() => handleClickSection(section.id)}>
-                            <p>{section.title}</p>
-                            <div className="sectionProgress">
-                                <p className="defaultStyle">
-                                    {t("completedTasks1")}
-                                    {section.completed_tasks}
-                                    {t("completedTasks2")}
-                                    {section.total_tasks} {t("completedTasks3")}
-                                </p>
-                                <progress value={section.percentage_completed} />
-                            </div>
-                            <Link to={`/dashboard/courses/${courseId}/sections/${section.id}/chapters`}>
-                                <button className="orangeButton"><PlayArrowIcon /></button>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                <Link
+                  to={`/dashboard/courses/${courseId}/sections/${section.id}/chapters`}
+                >
+                  <button className="orangeButton">
+                    <PlayArrowIcon />
+                  </button>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
     </div>
   );
 };
