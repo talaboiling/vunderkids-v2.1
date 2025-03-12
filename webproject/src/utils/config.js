f5f9d13044e42fe9cc8803e3c60f633ea599afa0
BekzhanK1 <bkimadieff@gmail.com>
2025-03-12 22:04:10 +0500

    change api url

--- webproject/src/utils/config.js
+++ webproject/src/utils/config.js
@@ -1,3 +1,3 @@
 // export const API_URL = "https://api.protosedu.kz/api";
-export const API_URL = "http://85.198.89.17:9000/api";
+export const API_URL = "https://api.protosedu.kz/api";
 // export const API_URL = "http://localhost:9000/api";
