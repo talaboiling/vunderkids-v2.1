6d4af9e70864e66176ee04abf741345ca6ceef1d
BekzhanK1 <bkimadieff@gmail.com>
2025-03-14 17:05:29 +0500

    script only on dashboard

--- webproject/src/App.jsx
+++ webproject/src/App.jsx
@@ -1,14 +1,27 @@
-import {Link} from "react-router-dom";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Contents from './Contents.jsx'
 import Reviews from "./Reviews.jsx";
 import Socials from "./components/Socials.jsx"
 import PopOlympiad from "./PopOlympiad.jsx";
-import React, {FC, Suspense} from 'react';
+import React, { FC, Suspense } from 'react';
 import './i18n.js';
 function App() {
 
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "//code.jivo.ru/widget/lx81Wuqx8B"
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+
+  }, []
+  )
   return (
     <>
       <div className="contain">
@@ -21,9 +34,9 @@ function App() {
           <Socials />
         </Suspense>
       </div>
-      
+
     </>
-    
+
   )
 }
 
