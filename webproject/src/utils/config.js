2aaa0b296be27d6a3aad26f7190e69d5e6e85857
BekzhanK1 <bkimadieff@gmail.com>
2025-03-14 17:07:49 +0500

    small fixes

--- webproject/src/utils/config.js
+++ webproject/src/utils/config.js
@@ -1,3 +1,3 @@
 // export const API_URL = "https://api.protosedu.kz/api";
-// export const API_URL = "https://api.protosedu.kz/api";
-export const API_URL = "http://localhost:9000/api";
+export const API_URL = "https://api.protosedu.kz/api";
+// export const API_URL = "http://localhost:9000/api";
